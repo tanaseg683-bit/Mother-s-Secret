54882d2da5783f2a9e58825e1f67f7ee4c9f3fe1
tanaseg683-bit <tanaseg683@gmail.com>
2026-09-17 09:56:55 +0300

    Add files via upload

---
+++ yaml.js
@@ -0,0 +1,42 @@
+------------------------------------------
+
+yaml.js
+------------------------------------------
+
+import express from "express";
+import yaml from "js-yaml";
+import fs from "fs";
+import { attachWebSocket } from "../websocket.js";
+
+const Router = express.Router();
+
+const isYaml = (filename) => filename.split(".").pop() === "yaml";
+
+Router.post("/", (req, res) => {
+  let file_path = req.body.file_path;
+  const filePath = `./public/${file_path}`;
+
+  if (!isYaml(filePath)) {
+    res.status(500).json({
+      status: "error",
+      message: "Not a YAML file path.",
+    });
+    return;
+  }
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).json({
+        status: "error",
+        message: "Failed to read the file.",
+      });
+      return;
+    }
+
+    res.status(200).send(yaml.load(data));
+
+    attachWebSocket().of("/yaml").emit("yaml", "YAML data has been processed.");
+  });
+});
+
+export default Router;
